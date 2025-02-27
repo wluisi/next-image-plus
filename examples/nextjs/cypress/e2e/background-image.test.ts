e2959bc769920cdb3050267e4eeb70eb5f34c6a6
wluisi <w.luisi@williamluisi.org>
2025-02-27 15:05:21 -0500

    make background test ts file and use shared utils

--- examples/nextjs/cypress/e2e/background-image.test.ts
+++ examples/nextjs/cypress/e2e/background-image.test.ts
@@ -1,3 +1,9 @@
+import {
+  checkLinkPreload,
+  encodeLikeNext,
+  interceptNextImage,
+} from "../support/utils";
+
 const FALLBACK_IMG = "https://picsum.photos/id/870/430/466";
 const MD_IMG = "https://picsum.photos/id/870/768/512";
 const LG_IMG = "https://picsum.photos/id/870/2560/800";
@@ -18,45 +24,6 @@ const LG_WIDTH_1X = "3840";
 const BASE_URL = Cypress.config().baseUrl;
 
 const SLUG = "/image/background";
-
-export function interceptNextImage(options) {
-  const { fallback, md, lg } = options;
-
-  cy.log("Intercept the network requests preloaded image(s)");
-  cy.intercept("GET", `/_next/image?url=${encodeURIComponent(fallback)}*`).as(
-    "fallback-image"
-  );
-  cy.intercept("GET", `/_next/image?url=${encodeURIComponent(md)}*`).as(
-    "md-image"
-  );
-  cy.intercept("GET", `/_next/image?url=${encodeURIComponent(lg)}*`).as(
-    "lg-image"
-  );
-}
-
-function encodeLikeNext(url) {
-  return url
-    .split("&") // Split at `&` to handle parameters separately
-    .map((part) => part.replace(/[^&=]/g, encodeURIComponent)) // Encode everything except `&` and `=`
-    .join("&"); // Rejoin the parts
-}
-
-function checkLinkPreload(options) {
-  const { img, media, width1x, width2x } = options;
-
-  cy.get(`link[rel="preload"][media="${media}"]`)
-    .should("have.attr", "as", "image")
-    .and("have.attr", "imagesrcset");
-
-  cy.get(`link[rel="preload"][media="${media}"]`)
-    .invoke("attr", "imagesrcset")
-    .then((imagesrcset) => {
-      const imagesrcsetDecoded = decodeURIComponent(imagesrcset);
-
-      expect(imagesrcsetDecoded).to.include(`${img}&w=${width1x}`);
-      expect(imagesrcsetDecoded).to.include(`${img}&w=${width2x}`);
-    });
-}
 
 describe("Fallback: next background image tests (pages)", () => {
   beforeEach(() => {
