12ec32ce80458a4992ef309751aaaca7e399ae74
wluisi <w.luisi@williamluisi.org>
2025-02-27 22:44:43 -0500

    try chrome

--- examples/nextjs/cypress.config.ts
+++ examples/nextjs/cypress.config.ts
@@ -4,21 +4,30 @@ export default defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on("task", {
-        log(message) {
-          console.log(message);
-
-          return null;
-        },
-        table(message) {
-          console.table(message);
-
-          return null;
-        },
+    setupNodeEvents(on, _config) {
+      on("before:browser:launch", (browser: any = {}, launchOptions) => {
+        if (browser.family === "chrome") {
+          launchOptions.args.push("--force-device-scale-factor=2");
+          launchOptions.args.push("--high-dpi-support=2");
+        }
+        return launchOptions;
       });
     },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    //   on("task", {
+    //     log(message) {
+    //       console.log(message);
+
+    //       return null;
+    //     },
+    //     table(message) {
+    //       console.table(message);
+
+    //       return null;
+    //     },
+    //   });
+    // },
     specPattern: "**/*.test.{js,jsx,ts,tsx}",
   },
 });
