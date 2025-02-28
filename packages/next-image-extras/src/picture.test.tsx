87c4d5bab5bddb0b69d6c61aa29b1903f07b5ac9
wluisi <w.luisi@williamluisi.org>
2025-02-28 15:28:26 -0500

    add basic tests for picture

--- packages/next-image-extras/src/picture.test.tsx
+++ packages/next-image-extras/src/picture.test.tsx
@@ -12,33 +12,61 @@ import { render, screen } from "@testing-library/react";
 
 import { Picture, Image, Source } from "./picture";
 
-describe("Picture tests", () => {
-  it("should return an image element if as is omitted", () => {
-    render(
-      <Picture preload={true}>
-        <Source
-          media="(min-width: 600px) and (max-width:959px)"
-          src="/images/medium.jpg"
-          width={400}
-          height={200}
-        />
-        <Source
-          media="(min-width: 960px)"
-          src="/images/large.jpg"
-          sizes="50vw"
-          width={600}
-          height={600}
-        />
-        <Image
-          src="/images/fallback.jpg"
-          width={600}
-          height={600}
-          alt="Mountains and a river"
-          className="h-full w-full rounded-md md:rounded-lg object-cover"
-        />
-      </Picture>
+const PictureComponentMock = (
+  <Picture preload={true}>
+    <Source
+      media="(min-width: 600px) and (max-width:959px)"
+      src="/images/medium.jpg"
+      width={400}
+      height={200}
+    />
+    <Source
+      media="(min-width: 960px)"
+      src="/images/large.jpg"
+      width={600}
+      height={600}
+    />
+    <Image
+      src="/images/fallback.jpg"
+      width={600}
+      height={600}
+      alt="Mountains and a river"
+      className="h-full w-full rounded-md md:rounded-lg object-cover"
+    />
+  </Picture>
+);
+
+describe("Picture component tests", () => {
+  it("should replace fallback image src with next image url.", () => {
+    const { container } = render(PictureComponentMock);
+
+    const img = container.querySelector("picture img");
+    expect(img).toHaveAttribute(
+      "src",
+      "/_next/image?url=%2Fimages%2Ffallback.jpg&w=1200&q=75"
     );
 
-    screen.debug();
+    // screen.debug();
+  });
+
+  it("should return correct source elements.", () => {
+    const { container } = render(PictureComponentMock);
+
+    const sourceMd = container.querySelector(
+      "picture source[media='(min-width: 600px) and (max-width:959px)']"
+    );
+
+    expect(sourceMd).toHaveAttribute(
+      "srcSet",
+      "/_next/image?url=%2Fimages%2Fmedium.jpg&w=640&q=75 1x, /_next/image?url=%2Fimages%2Fmedium.jpg&w=828&q=75 2x"
+    );
+
+    const sourceLarge = container.querySelector(
+      "picture source[media='(min-width: 960px)']"
+    );
+    expect(sourceLarge).toHaveAttribute(
+      "srcSet",
+      "/_next/image?url=%2Fimages%2Flarge.jpg&w=640&q=75 1x, /_next/image?url=%2Fimages%2Flarge.jpg&w=1200&q=75 2x"
+    );
   });
 });
