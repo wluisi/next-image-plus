93bda7fac877b72cb643a7d949e6d04d48e23d34
William Luisi <wluisi@users.noreply.github.com>
2025-03-03 12:54:58 -0500

    GH-22: Simplify Code (#23)
    * simplify code snippet
    
    * cleanup

---
+++ apps/www/src/components/components-map.tsx
@@ -0,0 +1,48 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  CodeSnippet,
+  Heading,
+  HeadingAnchorLink,
+} from "./../components/ui";
+
+/**
+ * Build a components map for use in compileMDX().
+ */
+export const componentMap = {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  // Overrides
+  h1(props: any) {
+    return <Heading level="h1" {...props} />;
+  },
+  h2(props: any) {
+    return <HeadingAnchorLink level="h2" title={props.children as string} />;
+  },
+  h3(props: any) {
+    return <HeadingAnchorLink level="h3" title={props.children as string} />;
+  },
+  pre(props: any) {
+    const code = props.children?.props?.children as string;
+    const language = props.children?.props?.className.replace("language-", "");
+
+    let codeFinal = "";
+    let title;
+
+    const firstLine = code.split("\n")[0];
+    const shortcode = "@filename:";
+
+    if (firstLine.startsWith(shortcode)) {
+      codeFinal = code.split("\n").slice(1).join("\n");
+      title = firstLine.split(shortcode)[1];
+    } else {
+      codeFinal = code;
+    }
+    // @ts-expect-error - fixes `Type error: 'CodeSnippet' cannot be used as a JSX component.`
+    return <CodeSnippet title={title} language={language} code={codeFinal} />;
+  },
+};
--- apps/www/src/utils/get-content.tsx
+++ apps/www/src/utils/get-content.tsx
@@ -8,21 +8,13 @@
 
 import fs from "fs";
 import path from "path";
-
 import { compileMDX } from "next-mdx-remote/rsc";
-import { notFound } from "next/navigation";
-
+// Utils
 import getTableOfContents from "./get-table-of-contents";
+// Components
+import { componentMap } from "./../components/components-map";
 
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  CodeSnippet,
-  Heading,
-  HeadingAnchorLink,
-} from "./../components/ui";
+import { notFound } from "next/navigation";
 
 export async function getContent(slug: string) {
   const filePath = path.join(process.cwd(), "src/__content", `${slug}.mdx`);
@@ -32,28 +24,7 @@ export async function getContent(slug: string) {
 
     const { content, frontmatter } = await compileMDX({
       source: mdxSource,
-      components: {
-        Accordion,
-        AccordionButton,
-        AccordionItem,
-        AccordionPanel,
-        // @ts-expect-error - fix me later
-        CodeSnippet,
-        Heading,
-        h1(props) {
-          return <Heading level="h1" {...props} />;
-        },
-        h2(props) {
-          return (
-            <HeadingAnchorLink level="h2" title={props.children as string} />
-          );
-        },
-        h3(props) {
-          return (
-            <HeadingAnchorLink level="h3" title={props.children as string} />
-          );
-        },
-      },
+      components: componentMap,
       options: {
         parseFrontmatter: true,
       },
