021af6ffcc55891c0a1b22285ef31be815689675
wluisi <w.luisi@williamluisi.org>
2025-03-03 12:44:41 -0500

    cleanup

--- apps/www/src/utils/get-content.tsx
+++ apps/www/src/utils/get-content.tsx
@@ -8,21 +8,13 @@
 
 import fs from "fs";
 import path from "path";
-
 import { compileMDX } from "next-mdx-remote/rsc";
-import { notFound } from "next/navigation";
-
+// Utils
 import getTableOfContents from "./get-table-of-contents";
+// Components
+import { componentMap } from "./../components/components-map";
 
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  CodeSnippet,
-  Heading,
-  HeadingAnchorLink,
-} from "./../components/ui";
+import { notFound } from "next/navigation";
 
 export async function getContent(slug: string) {
   const filePath = path.join(process.cwd(), "src/__content", `${slug}.mdx`);
@@ -32,54 +24,7 @@ export async function getContent(slug: string) {
 
     const { content, frontmatter } = await compileMDX({
       source: mdxSource,
-      components: {
-        Accordion,
-        AccordionButton,
-        AccordionItem,
-        AccordionPanel,
-        // @ts-expect-error - fix me later
-        CodeSnippet,
-        Heading,
-        h1(props) {
-          return <Heading level="h1" {...props} />;
-        },
-        h2(props) {
-          return (
-            <HeadingAnchorLink level="h2" title={props.children as string} />
-          );
-        },
-        h3(props) {
-          return (
-            <HeadingAnchorLink level="h3" title={props.children as string} />
-          );
-        },
-        pre(props) {
-          // @ts-expect-error - fuck off
-          const code = props.children?.props?.children as string;
-          // @ts-expect-error - fuck off
-          const language = props.children?.props?.className.replace(
-            "language-",
-            ""
-          );
-
-          let codeFinal = "";
-          let title;
-
-          const firstLine = code.split("\n")[0];
-          const shortcode = "@filename:";
-
-          if (firstLine.startsWith(shortcode)) {
-            codeFinal = code.split("\n").slice(1).join("\n");
-            title = firstLine.split(shortcode)[1];
-          } else {
-            codeFinal = code;
-          }
-
-          return (
-            <CodeSnippet title={title} language={language} code={codeFinal} />
-          );
-        },
-      },
+      components: componentMap,
       options: {
         parseFrontmatter: true,
       },
