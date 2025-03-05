cca971d0fb88b0bb6e780a48cdb5cc815fcc953b
wluisi <w.luisi@williamluisi.org>
2025-03-05 16:15:37 -0500

    fix mobile logo going 2 lines

--- apps/www/src/app/docs/[...slug]/page.tsx
+++ apps/www/src/app/docs/[...slug]/page.tsx
@@ -78,44 +78,40 @@ export default async function DocsSlugPage({
   ];
 
   return (
-    <div id="page-layout" className="container mx-auto px-8 py-4">
-      <main role="main">
-        <Grid
-          id="grid"
-          className="md:grid md:grid-cols-12 md:grid-flow-col gap-5 pb-10"
-        >
-          <GridItem id="left-sidebar" as="aside" className="md:col-span-2">
-            <SidebarMenu
-              id={sidebarMenu.id}
-              title={sidebarMenu.title}
-              menuItems={sidebarMenu.items}
-            />
-          </GridItem>
-          <GridItem id="main-content" className="md:col-span-8 pb-10 md:px-10">
-            <article className="space-y-5 prose">
-              <Breadcrumb className="m-auto max-w-xxl">
-                {breadcrumbs.map((item) => {
-                  return (
-                    <BreadcrumbItem key={item.url}>
-                      <BreadcrumbLink
-                        as={Link}
-                        href={item.url}
-                        isCurrentPage={item.isCurrentPage}
-                      >
-                        {item.title}
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                  );
-                })}
-              </Breadcrumb>
-              {page.content}
-            </article>
-          </GridItem>
-          <GridItem id="left-sidebar" as="aside" className="md:col-span-2">
-            <TableOfContents data={page.toc} />
-          </GridItem>
-        </Grid>
-      </main>
-    </div>
+    <Grid
+      id="grid"
+      className="md:grid md:grid-cols-12 md:grid-flow-col gap-5 pb-10"
+    >
+      <GridItem id="left-sidebar" as="aside" className="md:col-span-2">
+        <SidebarMenu
+          id={sidebarMenu.id}
+          title={sidebarMenu.title}
+          menuItems={sidebarMenu.items}
+        />
+      </GridItem>
+      <GridItem id="main-content" className="md:col-span-8 pb-10 md:px-10">
+        <article className="space-y-5 prose">
+          <Breadcrumb className="m-auto max-w-xxl">
+            {breadcrumbs.map((item) => {
+              return (
+                <BreadcrumbItem key={item.url}>
+                  <BreadcrumbLink
+                    as={Link}
+                    href={item.url}
+                    isCurrentPage={item.isCurrentPage}
+                  >
+                    {item.title}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              );
+            })}
+          </Breadcrumb>
+          {page.content}
+        </article>
+      </GridItem>
+      <GridItem id="left-sidebar" as="aside" className="md:col-span-2">
+        <TableOfContents data={page.toc} />
+      </GridItem>
+    </Grid>
   );
 }
