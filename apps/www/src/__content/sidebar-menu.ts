d9ad73e591ea685be66d46bdaf0a08e1e97c58d1
William Luisi <wluisi@users.noreply.github.com>
2025-03-05 12:41:38 -0500

    sidebar menu (#34)

---
+++ apps/www/src/__content/sidebar-menu.ts
@@ -0,0 +1,21 @@
+export const sidebarMenu = {
+  id: "sidebar-menu",
+  title: "Sidebar Menu Navigation",
+  items: [
+    {
+      id: "sidebar-menu-item-1",
+      url: "#introduction",
+      title: "Introduction",
+    },
+    {
+      id: "sidebar-menu-item-2",
+      url: "#picture-component",
+      title: "Picture Component",
+    },
+    {
+      id: "sidebar-menu-item-3",
+      url: "#background-image-component",
+      title: "Background Image Component",
+    },
+  ],
+};
