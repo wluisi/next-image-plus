2185fa761db1279ab2a0c0dd980a2f1be977d764
wluisi <w.luisi@williamluisi.org>
2025-03-05 15:14:48 -0500

    fixes for accordion spacing and colors

--- apps/www/src/components/content-components/Accordion.tsx
+++ apps/www/src/components/content-components/Accordion.tsx
@@ -28,12 +28,12 @@ export default function Accordion({
   return (
     <div id={id}>
       <Heading level="h2">{title}</Heading>
-      <p className="text-lg text-black/90 mb-3">{description}</p>
+      <p className="text-lg text-black/90 mb-5">{description}</p>
       {items.map((item: AccordionPropsItem) => {
         return (
           <SharedAccordion key={item.id}>
             <AccordionItem>
-              <h3 className="mb-0 text-lg">
+              <h3 className="mb-0 text-m">
                 <AccordionButton>{item.question}</AccordionButton>
               </h3>
               <AccordionPanel>{item.answer}</AccordionPanel>
--- apps/www/src/components/ui/Accordion/Accordion.tsx
+++ apps/www/src/components/ui/Accordion/Accordion.tsx
@@ -6,7 +6,7 @@ export interface AccordionProps extends React.HTMLAttributes<HTMLElement> {
 }
 
 export function Accordion({ className, children }: AccordionProps) {
-  let classNames = "accordion not-prose py-3";
+  let classNames = "accordion not-prose mb-4";
   if (className) {
     classNames = `${className} ${classNames}`;
   }
