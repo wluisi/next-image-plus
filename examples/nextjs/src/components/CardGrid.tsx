f49c0af5bf647adb950576edf98bb1f4e1f96e1a
wluisi <w.luisi@williamluisi.org>
2025-03-06 17:58:32 -0500

    make card grid shared across pages and app

---
+++ examples/nextjs/src/components/CardGrid.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Card from "./Card";
+import { Picture, Source, Image } from "next-image-extras";
+import { cardsContent } from "./../__content/cards";
+
+export default function CardGrid() {
+  return (
+    <div className="card-grid">
+      {cardsContent.map((item, index) => {
+        const image = item.image;
+        // Only preload the first card.
+        const preload = index === 0 ? true : false;
+
+        return (
+          <Card
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            image={
+              <Picture preload={preload} fallbackMedia="(max-width: 430px)">
+                <Source
+                  media="(min-width: 431px) and (max-width: 1023px)"
+                  src={image.medium.url}
+                  sizes="100vw"
+                  width={image.medium.width}
+                  height={image.medium.height}
+                />
+                <Source
+                  media="(min-width: 1024px)"
+                  src={image.large.url}
+                  width={image.large.width}
+                  height={image.large.height}
+                />
+                <Image
+                  src={image.fallback.url}
+                  width={image.fallback.width}
+                  height={image.fallback.height}
+                  alt={image.fallback.alt}
+                  className="object-cover"
+                />
+              </Picture>
+            }
+          />
+        );
+      })}
+    </div>
+  );
+}
