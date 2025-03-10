d43b5d146709e9028098190e957f008c53c4d3ec
wluisi <w.luisi@williamluisi.org>
2025-03-10 17:06:59 -0400

    gh-76: delete examples/nextjs app

--- examples/nextjs/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { AppProps } from "next/app";
-
-// Components
-import "./globals.css";
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
