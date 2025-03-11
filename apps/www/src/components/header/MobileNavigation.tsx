1202b738ebf455805a4d991844cbe240349ac73f
wluisi <w.luisi@williamluisi.org>
2025-03-11 16:34:32 -0400

    add border bottom for menu items

--- apps/www/src/components/header/MobileNavigation.tsx
+++ apps/www/src/components/header/MobileNavigation.tsx
@@ -33,41 +33,47 @@ export default function MobileNavigation() {
           </button>
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content
-            align="center"
-            className="popover-content inset-x-4 z-50 w-screen rounded-3xl p-5 ring-4 ring-zinc-900/5 bg-popover text-popover-foreground outline-none bg-white shadow-[0_0_0_12000px_rgba(0,0,0,0.85)] duration-150 data-[closed]:scale-95 data-[closed]:opacity-0 data-[open]:ease-out data-[closed]:ease-in"
-            // sideOffset={-10}
-            // alignOffset={5}
-            // style={{ boxShadow: "0 0 0 12000px rgba(0, 0, 0, 0.85)" }}
-          >
-            <div className="flex flex-row-reverse items-center justify-between mb-5">
-              <Popover.Close className="popover-close" aria-label="Close">
-                {/* Close */}
-                <CloseIcon className="h-4 w-4" />
-              </Popover.Close>
-              <h2 className="text-sm font-medium mb-0">Navigation</h2>
-            </div>
-            <div>
-              <NavigationMenu id="whatever">
-                <NavigationMenuList className="list-none pl-0">
-                  {sidebarMenu.items.map((item: MenuItem) => {
-                    return (
-                      <NavigationMenuItem key={item.id} className="pr-5 mb-5">
-                        <NavigationMenuLink
-                          as={Link}
-                          href={item.url}
-                          // Close popover when link is clicked
-                          onClick={() => setOpen(false)}
+          <>
+            <div className="popover-backdrop fixed inset-0 z-50 bg-zinc-800/40 backdrop-blur-sm duration-150 data-[closed]:opacity-0 data-[open]:ease-out data-[closed]:ease-in dark:bg-black/80" />
+            <Popover.Content
+              align="center"
+              className="popover-content inset-x-4 z-50 w-screen rounded-3xl p-6 ring-4 ring-zinc-900/5 bg-popover text-popover-foreground outline-none bg-white duration-600 data-[closed]:scale-95 data-[closed]:opacity-0 data-[open]:ease-in data-[closed]:ease-out"
+              // sideOffset={-10}
+              // alignOffset={5}
+              // style={{ boxShadow: "0 0 0 12000px rgba(0, 0, 0, 0.85)" }}
+            >
+              <div className="flex flex-row-reverse items-center justify-between mb-5">
+                <Popover.Close className="popover-close" aria-label="Close">
+                  {/* Close */}
+                  <CloseIcon className="h-4 w-4" />
+                </Popover.Close>
+                <h2 className="text-sm font-medium mb-0">Navigation</h2>
+              </div>
+              <div>
+                <NavigationMenu id="whatever">
+                  <NavigationMenuList className="list-none pl-0">
+                    {sidebarMenu.items.map((item: MenuItem) => {
+                      return (
+                        <NavigationMenuItem
+                          key={item.id}
+                          className="py-2 border-b border-slate-200"
                         >
-                          {item.title}
-                        </NavigationMenuLink>
-                      </NavigationMenuItem>
-                    );
-                  })}
-                </NavigationMenuList>
-              </NavigationMenu>
-            </div>
-          </Popover.Content>
+                          <NavigationMenuLink
+                            as={Link}
+                            href={item.url}
+                            // Close popover when link is clicked
+                            onClick={() => setOpen(false)}
+                          >
+                            {item.title}
+                          </NavigationMenuLink>
+                        </NavigationMenuItem>
+                      );
+                    })}
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </div>
+            </Popover.Content>
+          </>
         </Popover.Portal>
       </Popover.Root>
     </div>
