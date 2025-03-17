6aec14da69c4820ab9112e033e209094e87b6dd0
wluisi <w.luisi@williamluisi.org>
2025-03-17 11:39:37 -0400

    more code cleanup

--- packages/next-image-plus/src/background-image.tsx
+++ packages/next-image-plus/src/background-image.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import {
   getImageProps as getNextImageProps,
   type ImageProps as NextImageProps,
--- packages/next-image-plus/src/picture.tsx
+++ packages/next-image-plus/src/picture.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import { getImageProps as getNextImageProps } from "next/image";
 import {
   PreloadImageLink,
