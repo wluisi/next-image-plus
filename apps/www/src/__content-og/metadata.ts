e96e031be320baf25e92091b6e2cca3aeaa9d054
wluisi <w.luisi@williamluisi.org>
2025-06-01 17:26:32 -0400

    delete unused code

--- apps/www/src/__content-og/metadata.ts
+++
@@ -1,5 +0,0 @@
-export const metadataBase = {
-  url: "https://github.com/wluisi/next-image-plus",
-  siteName: "next-image-plus",
-  twitterHandle: "@wluisi",
-};
--- apps/www/src/components/ui/Card/CardBody.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-
-// @ts-expect-error - fix me later
-export function CardBody({ children }) {
-  return <div className="px-5 py-10">{children}</div>;
-}
