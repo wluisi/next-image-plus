be0ee5ad9fd7428fd46c7b279cec38653d48d6f3
William Luisi <wluisi@users.noreply.github.com>
2025-06-03 13:20:02 -0400

    gh-148: examples (#149)
    * gh-148: examples
    
    * add picture page

---
+++ examples/next/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid p-10">
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link href="/picture">Picture Component</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
