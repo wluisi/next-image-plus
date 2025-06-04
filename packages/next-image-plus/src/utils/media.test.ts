121445b5de3d772d26e1ac4ae4fe922f77890949
wluisi <w.luisi@williamluisi.org>
2025-06-04 12:17:06 -0400

    alt approach

--- packages/next-image-plus/src/utils/media.test.ts
+++ packages/next-image-plus/src/utils/media.test.ts
@@ -1,7 +1,7 @@
 import { getMediaQueries } from "./media";
 
 describe("getMediaQueries() tests", () => {
-  it("should return modified media queries without overlap.", () => {
+  it("should return modified media queries if there's overlap.", () => {
     const items = [
       {
         uuid: "source-https://picsum.photos/id/59/860/430",
@@ -19,11 +19,39 @@ describe("getMediaQueries() tests", () => {
 
     const mediaQueries = getMediaQueries(items);
 
+    // console.log("mediaQueries", mediaQueries);
+
     expect(mediaQueries).toEqual({
-      "img-https://picsum.photos/id/59/430/215": "(max-width: 430px)",
       "source-https://picsum.photos/id/59/860/430":
-        "(min-width: 431px) and (max-width: 1024px)",
-      "source-https://picsum.photos/id/59/220/220": "(min-width: 1025px)",
+        "(min-width: 431px) and (max-width: 1023px)",
+      "source-https://picsum.photos/id/59/220/220": "(min-width: 1024px)",
+      "img-https://picsum.photos/id/59/430/215": "(max-width: 430px)",
+    });
+  });
+
+  it("should return media queries as is if there's no overlap.", () => {
+    const items = [
+      {
+        uuid: "source-https://picsum.photos/id/59/860/430",
+        media: "(min-width: 431px) and (max-width: 1023px)",
+      },
+      {
+        uuid: "source-https://picsum.photos/id/59/220/220",
+        media: "(min-width: 1024px)",
+      },
+      {
+        uuid: "img-https://picsum.photos/id/59/430/215",
+        media: "(max-width: 430px)",
+      },
+    ];
+
+    const mediaQueries = getMediaQueries(items);
+
+    expect(mediaQueries).toEqual({
+      "source-https://picsum.photos/id/59/860/430":
+        "(min-width: 431px) and (max-width: 1023px)",
+      "source-https://picsum.photos/id/59/220/220": "(min-width: 1024px)",
+      "img-https://picsum.photos/id/59/430/215": "(max-width: 430px)",
     });
   });
 });
