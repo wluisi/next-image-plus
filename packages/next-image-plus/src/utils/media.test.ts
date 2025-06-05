e1df91cf3dd0abf38455b880b58755f2cd25ee65
wluisi <w.luisi@williamluisi.org>
2025-06-04 20:24:39 -0400

    update tests

--- packages/next-image-plus/src/utils/media.test.ts
+++ packages/next-image-plus/src/utils/media.test.ts
@@ -1,7 +1,7 @@
 import { getMediaQueries, getFallbackMediaQuery } from "./media";
 
 describe("getMediaQueries() tests", () => {
-  it("should return modified media queries if there's overlap.", () => {
+  it("should modify media queries with single overlap.", () => {
     const items = [
       {
         uuid: "source-https://picsum.photos/id/59/860/430",
@@ -11,26 +11,19 @@ describe("getMediaQueries() tests", () => {
         uuid: "source-https://picsum.photos/id/59/220/220",
         media: "(min-width: 1024px)",
       },
-      // {
-      //   uuid: "img-https://picsum.photos/id/59/430/215",
-      //   media: "(max-width: 430px)",
-      // },
     ];
 
     const mediaQueries = getMediaQueries(items);
-
-    // console.log("mediaQueries", mediaQueries);
 
     expect(mediaQueries).toEqual({
       "source-https://picsum.photos/id/59/860/430":
         "(min-width: 431px) and (max-width: 1023px)",
       "source-https://picsum.photos/id/59/220/220": "(min-width: 1024px)",
-      // "img-https://picsum.photos/id/59/430/215": "(max-width: 430px)",
       "img-fallback": "(max-width: 430px)",
     });
   });
 
-  it("should return media queries as is if there's no overlap.", () => {
+  it("should not modify media queries if there's no overlap.", () => {
     const items = [
       {
         uuid: "source-https://picsum.photos/id/59/860/430",
@@ -56,27 +49,7 @@ describe("getMediaQueries() tests", () => {
     });
   });
 
-  // it("should something something edge case.", () => {
-  //   const items = [
-  //     {
-  //       uuid: "/images/medium.jpg",
-  //       media: "(min-width: 600px) and (max-width:959px)",
-  //     },
-  //     {
-  //       uuid: "/images/large.jpg",
-  //       media: "(min-width: 960px)",
-  //     },
-  //     // {
-  //     //   uuid: "img-https://picsum.photos/id/59/430/215",
-  //     //   media: "(max-width: 430px)",
-  //     // },
-  //   ];
-
-  //   const mediaQueries = getMediaQueries(items);
-  //   console.log("mediaQueries edge", mediaQueries);
-  // });
-
-  it("should return modified media queries for hero example.", () => {
+  it("should modify media queries with multiple overlaps", () => {
     const items = [
       {
         uuid: "fallback-https://picsum.photos/id/870/430/466",
@@ -93,6 +66,7 @@ describe("getMediaQueries() tests", () => {
     ];
 
     const mediaQueries = getMediaQueries(items);
+
     expect(mediaQueries).toEqual({
       "fallback-https://picsum.photos/id/870/430/466": "(max-width: 430px)",
       "md-https://picsum.photos/id/870/768/512":
@@ -100,10 +74,60 @@ describe("getMediaQueries() tests", () => {
       "lg-https://picsum.photos/id/870/2560/800": "(min-width: 1024px)",
     });
   });
+
+  it("should not modify media queries with overlap if options: `modify` false.", () => {
+    const items = [
+      {
+        uuid: "fallback-https://picsum.photos/id/870/430/466",
+        media: "(max-width: 430px)",
+      },
+      {
+        uuid: "md-https://picsum.photos/id/870/768/512",
+        media: "(min-width: 430px) and (max-width: 1024px)",
+      },
+      {
+        uuid: "lg-https://picsum.photos/id/870/2560/800",
+        media: "(min-width: 1024px)",
+      },
+    ];
+
+    const mediaQueries = getMediaQueries(items, { modify: false });
+
+    expect(mediaQueries).toEqual({
+      "fallback-https://picsum.photos/id/870/430/466": "(max-width: 430px)",
+      "md-https://picsum.photos/id/870/768/512":
+        "(min-width: 430px) and (max-width: 1024px)",
+      "lg-https://picsum.photos/id/870/2560/800": "(min-width: 1024px)",
+    });
+  });
+
+  it("should modify media queries with single overlap and options: `fallback` media query.", () => {
+    const items = [
+      {
+        uuid: "source-https://picsum.photos/id/59/860/430",
+        media: "(min-width: 430px) and (max-width: 1024px)",
+      },
+      {
+        uuid: "source-https://picsum.photos/id/59/220/220",
+        media: "(min-width: 1024px)",
+      },
+    ];
+
+    const mediaQueries = getMediaQueries(items, {
+      fallback: "(max-width: 430px)",
+    });
+
+    expect(mediaQueries).toEqual({
+      "source-https://picsum.photos/id/59/860/430":
+        "(min-width: 431px) and (max-width: 1023px)",
+      "source-https://picsum.photos/id/59/220/220": "(min-width: 1024px)",
+      "img-fallback": "(max-width: 430px)",
+    });
+  });
 });
 
 describe("getFallbackMediaQuery() tests", () => {
-  it("should return a non overlap fallback media query.", () => {
+  it("should return a fallback media query that does not overlap.", () => {
     const items = [
       {
         uuid: "source-https://picsum.photos/id/59/860/430",
@@ -116,20 +140,10 @@ describe("getFallbackMediaQuery() tests", () => {
     ];
 
     const fallback = getFallbackMediaQuery(items);
-    // console.log("fallback", fallback);
 
     expect(fallback).toEqual({
       uuid: "img-fallback",
       media: "(max-width: 430px)",
     });
-
-    // console.log("mediaQueries", mediaQueries);
-
-    // expect(mediaQueries).toEqual({
-    //   "source-https://picsum.photos/id/59/860/430":
-    //     "(min-width: 431px) and (max-width: 1023px)",
-    //   "source-https://picsum.photos/id/59/220/220": "(min-width: 1024px)",
-    //   "img-https://picsum.photos/id/59/430/215": "(max-width: 430px)",
-    // });
   });
 });
