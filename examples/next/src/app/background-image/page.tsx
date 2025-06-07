0435bbf019af6e5f8df18e92f09adbacc25b51fa
wluisi <w.luisi@williamluisi.org>
2025-06-07 16:26:55 -0400

    add background image to examples

---
+++ examples/next/src/app/background-image/page.tsx
@@ -0,0 +1,78 @@
+import { BackgroundImage } from "next-image-plus";
+
+export default function BackgroundImagePage() {
+  return (
+    <div className="grid p-10 m-auto max-w-3xl">
+      <h1 className="mb-10">BackgroundImage Component</h1>
+
+      <div className="mb-10">
+        <h2>BackgroundImage Component</h2>
+        <div className="relative w-full h-[60vh] lg:h-[800px] overflow-hidden">
+          <BackgroundImage
+            id="examples__background-image"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            preload={true}
+            images={[
+              {
+                breakpoint: "fallback",
+                media: "(max-width: 430px)",
+                url: "https://picsum.photos/id/870/430/466",
+                width: 430,
+                height: 466,
+              },
+              {
+                breakpoint: "md",
+                media: "(min-width: 430px) and (max-width: 768px)",
+                url: "https://picsum.photos/id/870/768/512",
+                width: 768,
+                height: 512,
+              },
+              {
+                breakpoint: "lg",
+                media: "(min-width: 768px)",
+                url: "https://picsum.photos/id/870/2560/800",
+                width: 2560,
+                height: 800,
+              },
+            ]}
+          />
+        </div>
+      </div>
+
+      <div className="mb-10">
+        <h2>BackgroundImage Component with normalizeMediaQueries disabled.</h2>
+        <div className="relative w-full h-[60vh] lg:h-[800px] overflow-hidden">
+          <BackgroundImage
+            id="examples__background-image-two"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            preload={true}
+            normalizeMediaQueries={false}
+            images={[
+              {
+                breakpoint: "fallback",
+                media: "(max-width: 430px)",
+                url: "https://picsum.photos/id/700/430/466",
+                width: 430,
+                height: 466,
+              },
+              {
+                breakpoint: "md",
+                media: "(min-width: 430px) and (max-width: 768px)",
+                url: "https://picsum.photos/id/700/768/512",
+                width: 768,
+                height: 512,
+              },
+              {
+                breakpoint: "lg",
+                media: "(min-width: 768px)",
+                url: "https://picsum.photos/id/700/2560/800",
+                width: 2560,
+                height: 800,
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
