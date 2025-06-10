7a7f90bed332a63891a79ba8a4881b6c66bad130
William Luisi <wluisi@users.noreply.github.com>
2025-06-10 16:40:52 -0400

    gh-158: background image blog post draft (#171)
    * gh-158: background image blog post draft
    
    * 2nd draft of blog post
    
    * add responsive background images section
    
    * unpublish blog post for now

--- examples/next/src/app/background-image/page.tsx
+++ examples/next/src/app/background-image/page.tsx
@@ -7,30 +7,14 @@ export default function BackgroundImagePage() {
 
       <div className="mb-10">
         <h2>BackgroundImage Component</h2>
-        <div className="relative w-full h-[60vh] lg:h-[800px] overflow-hidden">
+        <p>Single background image with no media and no breakpoint.</p>
+        <div className="relative w-full h-[30vh] overflow-hidden">
           <BackgroundImage
             id="examples__background-image"
             className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-            preload={true}
             images={[
               {
-                breakpoint: "fallback",
-                media: "(max-width: 430px)",
-                url: "https://picsum.photos/id/870/430/466",
-                width: 430,
-                height: 466,
-              },
-              {
-                breakpoint: "md",
-                media: "(min-width: 430px) and (max-width: 768px)",
-                url: "https://picsum.photos/id/870/768/512",
-                width: 768,
-                height: 512,
-              },
-              {
-                breakpoint: "lg",
-                media: "(min-width: 768px)",
-                url: "https://picsum.photos/id/870/2560/800",
+                url: "https://picsum.photos/id/502/2560/800",
                 width: 2560,
                 height: 800,
               },
@@ -40,34 +24,116 @@ export default function BackgroundImagePage() {
       </div>
 
       <div className="mb-10">
-        <h2>BackgroundImage Component with normalizeMediaQueries disabled.</h2>
+        <h2>BackgroundImage Component With Preload</h2>
+        <p>Single background image with no media and no breakpoint.</p>
+        <div className="relative w-full h-[30vh] overflow-hidden">
+          <BackgroundImage
+            id="examples__background-image-preload"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            images={[
+              {
+                url: "https://picsum.photos/id/502/2560/800",
+                width: 2560,
+                height: 800,
+              },
+            ]}
+            preload={true}
+          />
+        </div>
+      </div>
+
+      <div className="mb-10">
+        <h2>BackgroundImage Component (Responsive)</h2>
         <div className="relative w-full h-[60vh] lg:h-[800px] overflow-hidden">
           <BackgroundImage
-            id="examples__background-image-two"
+            id="examples__background-image-responsive"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            preload={true}
+            images={[
+              {
+                url: "https://picsum.photos/id/870/430/466",
+                media: "(max-width: 430px)",
+                width: 430,
+                height: 466,
+              },
+              {
+                url: "https://picsum.photos/id/870/768/512",
+                media: "(min-width: 430px) and (max-width: 768px)",
+                width: 768,
+                height: 512,
+              },
+              {
+                // breakpoint: "lg",
+                url: "https://picsum.photos/id/870/2560/800",
+                media: "(min-width: 768px)",
+                width: 2560,
+                height: 800,
+              },
+            ]}
+          />
+        </div>
+      </div>
+
+      <div className="mb-10">
+        <h2>
+          BackgroundImage Component (Responsive) with normalizeMediaQueries
+          disabled.
+        </h2>
+        <div className="relative w-full h-[60vh] lg:h-[800px] overflow-hidden">
+          <BackgroundImage
+            id="examples__background-image-responsive-two"
             className="absolute inset-0 bg-cover bg-center bg-no-repeat"
             preload={true}
             normalizeMediaQueries={false}
             images={[
               {
-                breakpoint: "fallback",
                 media: "(max-width: 430px)",
                 url: "https://picsum.photos/id/700/430/466",
                 width: 430,
                 height: 466,
               },
               {
-                breakpoint: "md",
                 media: "(min-width: 430px) and (max-width: 768px)",
                 url: "https://picsum.photos/id/700/768/512",
                 width: 768,
                 height: 512,
               },
               {
-                breakpoint: "lg",
                 media: "(min-width: 768px)",
                 url: "https://picsum.photos/id/700/2560/800",
                 width: 2560,
                 height: 800,
+              },
+            ]}
+          />
+        </div>
+      </div>
+
+      <div className="mb-10">
+        <h2>BackgroundImage Component Responsive</h2>
+        <p></p>
+        <div className="relative w-full h-[30vh] overflow-hidden">
+          <BackgroundImage
+            id="examples__responsive-background-image"
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            images={[
+              {
+                url: "https://picsum.photos/id/502/430/430",
+                media: "(max-width: 430px)",
+                width: 430,
+                height: 430,
+              },
+              {
+                url: "https://picsum.photos/id/502/768/768",
+                media: "(min-width: 430px) and (max-width: 768px)",
+                width: 768,
+                height: 768,
+              },
+              {
+                url: "https://picsum.photos/id/502/1024/1024",
+                media: "(min-width: 768px)",
+                width: 1024,
+                height: 1024,
               },
             ]}
           />
---
+++ examples/next/src/app/vanilla-next/background-image/page.tsx
@@ -0,0 +1,37 @@
+import { getImageProps } from "next/image";
+
+export default function VanillaNextBackgroundImagePage() {
+  const bgImage = getImageProps({
+    alt: "",
+    src: "https://picsum.photos/id/502/2560/800",
+    width: 2560,
+    height: 800,
+    // Has no effect.
+    priority: true,
+  });
+
+  const image = getImageProps({
+    alt: "",
+    src: "https://picsum.photos/id/123/2560/800",
+    width: 2560,
+    height: 800,
+    priority: true,
+  });
+
+  return (
+    <div className="grid p-10 m-auto max-w-3xl">
+      <h1 className="mb-10">Vanilla Next.js: Background Image</h1>
+      <div className="relative w-full h-[50vh] overflow-hidden">
+        <div
+          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{
+            backgroundImage: `url(${bgImage.props.src})`,
+          }}
+        />
+      </div>
+
+      <br />
+      <img {...image.props} />
+    </div>
+  );
+}
