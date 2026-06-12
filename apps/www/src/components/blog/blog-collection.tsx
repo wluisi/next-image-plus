d10764e69a79d91034a5adf3273c7b67fbf877a1
wluisi <w.luisi@williamluisi.org>
2026-06-12 12:51:00 -0400

    delete breadcrumb and remove gql from blog collection

--- apps/www/src/components/blog/blog-collection.tsx
+++ apps/www/src/components/blog/blog-collection.tsx
@@ -2,84 +2,33 @@
 
 import * as React from "react";
 
-import { client } from "../../graphinery";
-
 import { SimpleGrid, FeaturedLink } from "@graphinery/ui";
 import Link from "next/link";
 
-import { graphql, ResultOf, VariablesOf } from "./../../types";
+import { getCollection } from "./../../cc/collection";
 
-const BLOG_COLLECTION_QUERY = graphql(`
-  query BlogCollectionQuery(
-    $limit: Int
-    $pageNumber: Int
-    $sort: _QuerySort
-    $filter: BlogQueryFilter
-  ) {
-    blogCollection(
-      limit: $limit
-      pageNumber: $pageNumber
-      sort: $sort
-      filter: $filter
-    ) {
-      items {
-        id
-        title
-        path
-        description
-        status
-      }
-      pageInfo {
-        totalItems
-        limit
-        pageCount
-        pageNumber
-      }
-    }
-  }
-`);
-
-export type BlogCollection = ResultOf<
-  typeof BLOG_COLLECTION_QUERY
->["blogCollection"];
-type BlogCollectionData = { data: ResultOf<typeof BLOG_COLLECTION_QUERY> };
-type BlogCollectionVariables = VariablesOf<typeof BLOG_COLLECTION_QUERY>;
-
-async function getBlogCollection(): Promise<BlogCollection> {
-  const { data } = await client.request<
-    BlogCollectionData,
-    BlogCollectionVariables
-  >({
-    query: BLOG_COLLECTION_QUERY,
-    variables: {
-      limit: 10,
-      filter: {
-        status: { _eq: true },
-      },
+export default async function BlogCollection() {
+  const blogCollection = getCollection("blog", {
+    filter: {
+      status: { _eq: true },
     },
   });
 
-  return data.blogCollection;
-}
-
-export default async function BlogCollection() {
-  const blogCollection = await getBlogCollection();
-
-  if (!blogCollection || !blogCollection.items) {
+  if (!blogCollection) {
     return null;
   }
 
   return (
     <SimpleGrid id="blog-collection">
-      {blogCollection.items.map((item) => {
+      {blogCollection.map((item) => {
         if (!item) {
           return null;
         }
 
         return (
           <FeaturedLink
-            key={item.id}
-            href={item.path}
+            key={item._path}
+            href={item._path}
             as={Link}
             className="no"
             size="lg"
